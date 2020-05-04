4a594692e38eb0d2cbfe8117b5bc570e00dd446b
alexcslaguna@gmail.com <alexcslaguna@gmail.com>
2020-05-03 20:30:09 -0700

    commit primer

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -9,4 +9,36 @@ export class HomePage {
 
   constructor() {}
 
+  instrumentos = [
+
+{
+  nombre: 'flauta',
+  imagen: 'assets/imagenes/flauta.png',
+  audio: 'assets/imagenes/flauta.mp3'
+},
+
+{
+  nombre: 'bombo1',
+  imagen: 'assets/imagenes/bombo1.png',
+  audio: 'assets/imagenes/bombo1.mp3'
+},
+
+{
+  nombre: 'bombo2',
+  imagen: 'assets/imagenes/bombo2.png',
+  audio: 'assets/imagenes/bombo2.mp3'
+},
+
+
+{
+  nombre: 'platillos',
+  imagen: 'assets/imagenes/platillos.png',
+  audio: 'assets/imagenes/platillos.mp3'
+}
+
+
+
+
+  ];
+
 }
